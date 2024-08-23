70e53e45c0d4523233b2d5daa9fd05225d53dff8
Arthur rios <149263633+Acdoom@users.noreply.github.com>
2024-08-22 21:14:43 -0300

    Add files via upload

---
+++ src/Components/Description.js
@@ -0,0 +1,10 @@
+// src/components/Description.js
+import styled from 'styled-components';
+
+const Description = styled.p`
+  text-align: center;
+  margin-top: 10px;
+  color: #555;
+`;
+
+export default Description;
---
+++ src/Components/Image.js
@@ -0,0 +1,9 @@
+// src/components/Image.js
+import styled from 'styled-components';
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+export default Image;
